dd94e69f69db1f23dba744bacd264cd6e09ab9c7
sachinkumarsingh <sachinkumarsingh@localhost>
2025-04-14 11:02:19 +0530

    initial commit

---
+++ AdditionSubdtractionApp/src/main/java/com/incapp/servlets/SimpleInterest.java
@@ -0,0 +1,24 @@
+package com.incapp.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SimpleInterest
+ */
+@WebServlet("/SimpleInterest")
+public class SimpleInterest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
